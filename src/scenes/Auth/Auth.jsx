a2c938f9b58ce24de43b79d61408d25c725c54a7
WebTarantul <tarstar1990@gmail.com>
2019-11-21 14:10:59 +0200

    header to light

--- src/scenes/Auth/Auth.jsx
+++ src/scenes/Auth/Auth.jsx
@@ -1,15 +1,15 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Register from 'src/components/Register/Register';
 import Login from 'src/components/Login/Login';
 import Header from 'src/components/Header/Header';
 import withFooter from 'src/components/HOCs/withFooter/withFooter';
 import ResetPassword from 'src/components/ResetPassword/ResetPassword';
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
 import { routes } from '../routes';
 
 const Auth = () => (
   <div className="AuthWrapper">
-    <Header isWhite />
+    <Header isLight />
     <Switch>
       <Route path={routes.register} component={Register} />
       <Route path={routes.login} component={Login} />
